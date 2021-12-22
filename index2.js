0311fc01de7eb7601cd1f094d9f21b8ef110bd95
Yevhen <relog0077@gmail.com>
2021-12-22 12:27:18 +0200

    решал задачу на добавления функции

--- index2.js
+++ index2.js
@@ -1,9 +1,3 @@
-function summa(arr){
-    var sum = 0;
-    for (var i = 0; i <arr.length; i++)
-    sum += arr[i];
-    console.log(sum)
+function isValid(name) {
+  return !!name && !name.includes(' ') && name.length >= 4;
 }
-var  array = [1, 1, 2, 5];
-
-summa (array);
